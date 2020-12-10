2c96e7ef2513cf0e09cf93cf6a855b2c6a2f5657
AkhileshNS <nsakhilesh02@gmail.com>
2020-12-10 12:09:45 +0530

    Changed var to let

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -2,7 +2,7 @@ import {protractor, element, browser} from 'protractor';
 
 const commonUtils = () => ({
 	waitAndClick(locator) {
-		var EC = protractor.ExpectedConditions;	
+		let EC = protractor.ExpectedConditions;	
 		let expectedElement = element(locator);
 		browser.wait(EC.presenceOf(expectedElement), 15000);
 		expectedElement.click();
